f7bd43479ea86d494bdf49a4d35141498584c93d
Chris Tyson <CTyson@veracode.com>
2021-11-16 15:23:48 -0500

    Beta 0.1

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-"# check_pass_fail" 
+# Veracode Check Pass Fail
+
+A simple example script to check pass/fail status of a Veracode app profile (or sandbox).
+
+## Setup
+
+Clone this repository:
+
+    git clone https://github.com/christyson/check_pass_fail
+
+Install dependencies:
+
+    cd check_pass_fail
+    pip install -r requirements.txt
+
+(Optional) Save Veracode API credentials in `~/.veracode/credentials`
+
+    [default]
+    veracode_api_key_id = <YOUR_API_KEY_ID>
+    veracode_api_key_secret = <YOUR_API_KEY_SECRET>
+
+## usage
+
+usage: check_pass_fail.py [-h] -a APP [-s SANDBOX] [--delete]
+
+Note: at a minimum APP is required.  
+
+## Run
+
+If you have saved credentials as above you can run:
+
+    python check_pass_fail.py -a <your app name>
+    or
+    python check_pass_fail.py -a <your app name> -s <your sandbox name>
+
+To delete use the following commands:
+
+    python check_pass_fail.py -a <your app name> --delete
+    or
+    python check_pass_fail.py -a <your app name> -s <your sandbox name> --delete
+
+
+Otherwise you will need to set environment variables before running `example.py`:
+
+    export VERACODE_API_KEY_ID=<YOUR_API_KEY_ID>
+    export VERACODE_API_KEY_SECRET=<YOUR_API_KEY_SECRET>
+    python check_pass_fail.py -a <your app name>
+    or
+    python check_pass_fail.py -a <your app name> -s <your sandbox name>
---
+++ check_pass_fail.py
@@ -0,0 +1,54 @@
+import sys
+import requests
+import argparse
+from lxml import etree
+import datetime as dt
+from dateutil.parser import parse
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+from veracode_api_py import VeracodeAPI
+api_target = "https://analysiscenter.veracode.com/api/5.0/deletebuild.do"
+headers = {"User-Agent": "Python HMAC Example"}
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='This script checks to see if an app is currently building a policy scan and returns 0 if not and 1 otherwise or if the --delete flag is set it will delete that build if you have permissions and return a 0 if the build is deleted and 1 if the build is not deleted. Note: Sandbox is optional')
+    parser.add_argument('-a', '--app', help='App name to check',required=True)
+    parser.add_argument('-s', '--sandbox', default="", help='Sandbox name to check',required=False)
+    parser.add_argument('--delete',default=False, action='store_true', help='If set to True and a build is running it will be deleted', required=False)
+    args = parser.parse_args()
+
+    data = VeracodeAPI().get_app_list()
+    results = etree.fromstring(data)
+    found = False
+    for app in results:
+        if (app.attrib["app_name"] == args.app) and (args.sandbox == ""):
+           found = True
+           build_info = VeracodeAPI().get_build_info(app.attrib["app_id"])
+           info=etree.fromstring(build_info)
+           for child in info :
+             time=parse(child.attrib["policy_updated_date"])
+             print("App: "+args.app +" build: "+child.attrib["build_id"]+ " on: " +str(time.date()) +" at: " +str(time.time())+" policy compliance is: " + child.attrib["policy_compliance_status"])
+           exit(0)
+        if (app.attrib["app_name"] == args.app) and (args.sandbox != ""):
+           sandbox_list=VeracodeAPI().get_sandbox_list(app.attrib["app_id"])
+           sandboxes = etree.fromstring(sandbox_list)
+           for sandbox in sandboxes:
+              if sandbox.attrib["sandbox_name"] == args.sandbox:
+                 found = True
+                 build_info = VeracodeAPI().get_build_info(app.attrib["app_id"],"0",sandbox.attrib["sandbox_id"])
+                 info=etree.fromstring(build_info)
+                 for child in info :
+                    time=parse(child.attrib["launch_date"])
+                    print("App: "+args.app +"Sandbox: "+args.sandbox +" build: "+child.attrib["build_id"]+ " on: " +str(time.date()) +" at: " +str(time.time())+" policy compliance is: " + child.attrib["policy_compliance_status"])
+                    exit(0)
+                 print ('No completed scans in sandbox: '+args.sandbox+' of app: '+args.app)
+                 exit(0)
+    if (not found) and (args.sandbox == ""):
+       print ('App: '+args.app+' does not exist')
+    elif (not found) and (args.sandbox != ""):
+       print ('App: '+args.app+' with sandbox: '+args.sandbox+' does not exist')
+    exit(0)
+
+if __name__ == '__main__':
+    main()
